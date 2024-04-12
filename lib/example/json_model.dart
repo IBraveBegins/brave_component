cdc52b009c35400d6604e803188729cdefe2ed06
brave <ibravebegins@gmail.com>
2024-04-12 17:36:49 +0800

    brave_component 首次提交 1.序列化 2.多语言

--- lib/example/json_model.dart
+++ lib/example/json_model.dart
@@ -1,6 +1,6 @@
 import '../models/user.dart';
 
 void main() {
-  var u = User.fromJson({"name": "Jack", "age": 16, "+1": 20});
+  var u = User.fromJson({"name": "Brave", "age": 18, "+1": 20});
   print(u.loved); // 20
 }
